c01f1d06262d998accf40e70cbd0fe6ebe1639cf
Kmil0000z <41449113+Kmil0000z@users.noreply.github.com>
2018-08-30 17:56:42 -0500

    f

---
+++ DemoMenuMasterDetail/DemoMenuMasterDetail/ContactosPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DemoMenuMasterDetail
+{
+    public partial class ContactosPage : ContentPage
+    {
+        public ContactosPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DemoMenuMasterDetail/DemoMenuMasterDetail/MasterPageItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace DemoMenuMasterDetail
+{
+    public class MasterPageItem
+    {
+        public MasterPageItem()
+        {
+            public string Titulo { get; set; }
+            public string Icono { get; set; }
+            public Type TipoPagina { get; set; }
+        }
+    }
+}
